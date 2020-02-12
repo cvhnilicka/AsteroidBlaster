e690ed67c3234c0b83503805891be2de9dd58cd3
cvhnilicka <cvhnilicka@wisc.edu>
2020-02-11 16:02:02 -0800

    updated input controller, added player controller system, and updated statecomponent as well

--- core/src/com/cormicopiastudios/asteroidblaster/GameEngine/Controllers/InputController.java
+++ core/src/com/cormicopiastudios/asteroidblaster/GameEngine/Controllers/InputController.java
@@ -1,16 +1,60 @@
 package com.cormicopiastudios.asteroidblaster.GameEngine.Controllers;
 
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 
 public class InputController implements InputProcessor {
+
+    public boolean left,right,up,down;
+
+
     @Override
     public boolean keyDown(int keycode) {
-        return false;
+        boolean keyProcessed = false;
+        switch (keycode) // switch code base on the variable keycode
+        {
+            case Keys.LEFT:  	// if keycode is the same as Keys.LEFT a.k.a 21
+                left = true;	// do this
+                keyProcessed = true;// we have reacted to a keypress
+                break;
+            case Keys.RIGHT: 	// if keycode is the same as Keys.LEFT a.k.a 22
+                right = true;	// do this
+                keyProcessed = true;// we have reacted to a keypress
+                break;
+            case Keys.UP: 		// if keycode is the same as Keys.LEFT a.k.a 19
+                up = true;		// do this
+                keyProcessed = true;// we have reacted to a keypress
+                break;
+            case Keys.DOWN: 	// if keycode is the same as Keys.LEFT a.k.a 20
+                down = true;	// do this
+                keyProcessed = true;// we have reacted to a keypress
+        }
+        return keyProcessed;	//  return our peyProcessed flag
     }
 
     @Override
     public boolean keyUp(int keycode) {
-        return false;
+        boolean keyProcessed = false;
+        switch (keycode) // switch code base on the variable keycode
+        {
+            case Keys.LEFT:  	// if keycode is the same as Keys.LEFT a.k.a 21
+                left = false;	// do this
+                keyProcessed = true;	// we have reacted to a keypress
+                break;
+            case Keys.RIGHT: 	// if keycode is the same as Keys.LEFT a.k.a 22
+                right = false;	// do this
+                keyProcessed = true;	// we have reacted to a keypress
+                break;
+            case Keys.UP: 		// if keycode is the same as Keys.LEFT a.k.a 19
+                up = false;		// do this
+                keyProcessed = true;	// we have reacted to a keypress
+                break;
+            case Keys.DOWN: 	// if keycode is the same as Keys.LEFT a.k.a 20
+                down = false;	// do this
+                keyProcessed = true;	// we have reacted to a keypress
+        }
+        return keyProcessed;	//  return our peyProcessed flag
     }
 
     @Override
---
+++ core/src/com/cormicopiastudios/asteroidblaster/GameEngine/Systems/PlayerControlSystem.java
@@ -0,0 +1,65 @@
+package com.cormicopiastudios.asteroidblaster.GameEngine.Systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.MathUtils;
+import com.cormicopiastudios.asteroidblaster.GameEngine.Components.B2BodyComponent;
+import com.cormicopiastudios.asteroidblaster.GameEngine.Components.PlayerComponent;
+import com.cormicopiastudios.asteroidblaster.GameEngine.Components.StateComponent;
+import com.cormicopiastudios.asteroidblaster.GameEngine.Controllers.InputController;
+
+public class PlayerControlSystem extends IteratingSystem {
+
+    ComponentMapper<PlayerComponent> pm;
+    ComponentMapper<B2BodyComponent> b2m;
+    ComponentMapper<StateComponent> sm;
+
+    InputController controller;
+
+    @SuppressWarnings("unchecked")
+    public PlayerControlSystem(InputController controller) {
+        super(Family.all(PlayerComponent.class).get());
+        this.controller = controller;
+        pm = ComponentMapper.getFor(PlayerComponent.class);
+        b2m = ComponentMapper.getFor(B2BodyComponent.class);
+        sm = ComponentMapper.getFor(StateComponent.class);
+    }
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        B2BodyComponent b2body = b2m.get(entity);
+        StateComponent state = sm.get(entity);
+
+        if(b2body.body.getLinearVelocity().y > 0){
+            state.set(StateComponent.STATE_FALLING);
+        }
+
+        if(b2body.body.getLinearVelocity().y == 0){
+            if(state.get() == StateComponent.STATE_FALLING){
+                state.set(StateComponent.STATE_NORMAL);
+            }
+            if(b2body.body.getLinearVelocity().x != 0){
+                state.set(StateComponent.STATE_MOVING);
+            }
+        }
+
+        if(controller.left){
+            b2body.body.setLinearVelocity(MathUtils.lerp(b2body.body.getLinearVelocity().x, -5f, 0.2f),b2body.body.getLinearVelocity().y);
+        }
+        if(controller.right){
+            b2body.body.setLinearVelocity(MathUtils.lerp(b2body.body.getLinearVelocity().x, 5f, 0.2f),b2body.body.getLinearVelocity().y);
+        }
+
+        if(!controller.left && ! controller.right){
+            b2body.body.setLinearVelocity(MathUtils.lerp(b2body.body.getLinearVelocity().x, 0, 0.1f),b2body.body.getLinearVelocity().y);
+        }
+
+        if(controller.up &&
+                (state.get() == StateComponent.STATE_NORMAL || state.get() == StateComponent.STATE_MOVING)){
+            //b2body.body.applyForceToCenter(0, 3000,true);
+            b2body.body.applyLinearImpulse(0, 75f, b2body.body.getWorldCenter().x,b2body.body.getWorldCenter().y, true);
+            state.set(StateComponent.STATE_JUMPING);
+        }
+    }
+}
